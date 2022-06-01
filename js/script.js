13fd2cd9e8386236a2cea2d5d5b5f50ef426c322
Luketto-dev <luca.francesconi1995@gmail.com>
2022-06-01 16:19:58 +0200

    esercizio completato

---
+++ js/script.js
@@ -0,0 +1,76 @@
+//Per la pagina ‘Chi siamo’ del nostro sito aziendale, dobbiamo sviluppare la sezione “Il nostro team”.
+//Definiamo un array di oggetti che rappresentano i membri del team.
+//Ogni membro ha le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+//Milestone 1:
+//stampare in console l’elenco dei membri del team, scrivendo separatamente i dettagli di ciascun componente.
+//Milestone 2:
+//stampare i dati all’interno di un contenitore nella pagina html in modo dinamico, creando per ciascun membro del team 
+//un elemento html che conterrà i suoi dati.
+
+// creo array con all interno oggetti che rappresentano i membri del team
+const team = [
+    {
+        nome: "Luca",
+        ruolo: "Front-End Developer",
+        foto: "img/new-team-member-02.jpg"
+    },
+    {
+        nome: "ludovica",
+        ruolo: "Dirigente",
+        foto: "img/angela-caroll-chief-editor.jpg"
+    },
+    {
+        nome: "Barbara",
+        ruolo: "Graphic Designer",
+        foto: "img/barbara-ramos-graphic-designer.jpg"
+    },
+    {
+        nome: "Federico",
+        ruolo: "Consulente",
+        foto: "img/walter-gordon-office-manager.jpg"
+    },
+    {
+        nome: "Silvia",
+        ruolo: "Segretaria",
+        foto: "img/new-team-member-03.jpg"
+
+    },
+    {
+        nome: "Paolo",
+        ruolo: "Back-end Developer",
+        foto: "img/scott-estrada-developer.jpg"
+
+    }
+]
+
+// mi vado a prendere il contenitore dove andro a inserire tutti i membri
+const teamContainer = document.querySelector(".team-container")
+
+// ciclo sull aray team
+for (let i = 0; i < team.length; i++) {
+    let membro = team[i]
+    // stampo in console nome, ruolo, foto di ogni membro
+    console.log(membro.nome, membro.ruolo, membro.foto)
+
+    // creo il template per inserirlo nell html
+    let template = `<div class="team-card">
+                        <div class="card-image">
+                        <img
+                            src= "${membro.foto}"
+                            alt="Wayne Barnett"
+                        />
+                        </div>
+                        <div class="card-text">
+                        <h3>${membro.nome}</h3>
+                        <p>${membro.ruolo}</p>
+                        </div>
+                    </div>`
+
+    console.log(template)
+
+    // faccio innerHtml del template
+    teamContainer.innerHTML += template
+
+
+    
+}
